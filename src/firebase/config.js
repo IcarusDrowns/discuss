eaa388f99847e07a63f86e2b0b39ef075384055e
IcarusDrowns <73529048+IcarusDrowns@users.noreply.github.com>
2022-02-08 10:32:50 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyCar6GM87c2UA-gb9-svacj-jzv3KJ-x5E",
-  authDomain: "projectmanagement-826fb.firebaseapp.com",
-  projectId: "projectmanagement-826fb",
-  storageBucket: "projectmanagement-826fb.appspot.com",
-  messagingSenderId: "518315173907",
-  appId: "1:518315173907:web:3247b9e7696577512337e6",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 firebase.initializeApp(firebaseConfig); //initialising the application @Amritanshu
 
